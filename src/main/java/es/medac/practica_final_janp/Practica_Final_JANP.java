992dbc26c2fa57b3c5fbe67ff6a3bc3dc4155f23
Antonio <aev0004@alu.medac.es>
2023-05-09 17:00:37 +0200

    Actualizacion paquetes

--- src/main/java/es/medac/practica_final_janp/Practica_Final_JANP.java
+++ src/main/java/es/medac/practica_final_janp/Practica_Final_JANP.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.practica_final_janp;
+package es.medac.practica_final_janp;
 
 /**
  *
