156dd3ef5def226415d4e0efde77687588ea19d0
Antonio <aev0004@alu.medac.es>
2023-05-09 17:56:55 +0200

    Paquetes interfaces y bd creados

---
+++ src/main/java/es/medac/practica_final_janp/bd/ConexionSQL.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.medac.practica_final_janp.bd;
+
+/**
+ *
+ * @author pipoa
+ */
+public class ConexionSQL {
+    
+}
