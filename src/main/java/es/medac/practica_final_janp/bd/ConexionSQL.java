5a295ccb383e8ddfc1fe08f0aa06db7c7c281004
pepesk34 <pepevic.dalias@gmail.com>
2023-05-16 16:33:33 +0200

    cambio Conexion

--- src/main/java/es/medac/practica_final_janp/bd/ConexionSQL.java
+++ src/main/java/es/medac/practica_final_janp/bd/ConexionSQL.java
@@ -4,10 +4,44 @@
  */
 package es.medac.practica_final_janp.bd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author pipoa
  */
 public class ConexionSQL {
-    
+    protected Connection conexion;
+    private final String URL = "jdbc:mysql://localhost/proyectoentornos";
+    private final String USUARIO = "root";
+    private final String PASSWORD = "";
+
+    /**
+     *
+     */
+    public ConexionSQL() {
+        try {
+            conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+            //System.out.println("Conectado");
+        } catch (SQLException e) {
+            System.out.println("Error al abrir la conexion "+e);
+        }
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void cerrar() {
+        try {
+            if (conexion != null && !conexion.isClosed()) {
+                conexion.close();
+               // System.out.println("Se ha cerrado la conexión a la BD.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al cerrar la conexion "+e);
+        }
+    }
 }
