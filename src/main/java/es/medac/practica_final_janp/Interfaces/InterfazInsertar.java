eebb603db45c4f52c46d3d3271b4d76d610c1b80
Antonio <aev0004@alu.medac.es>
2023-05-24 16:45:13 +0200

    falta modificar interfaz

--- src/main/java/es/medac/practica_final_janp/Interfaces/InterfazInsertar.java
+++ src/main/java/es/medac/practica_final_janp/Interfaces/InterfazInsertar.java
@@ -62,6 +62,8 @@ public class InterfazInsertar extends javax.swing.JFrame {
         jTReparacion_tiempo = new javax.swing.JTextField();
         jLReparacion_total = new javax.swing.JLabel();
         jTReparacion_total = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(500, 500));
@@ -154,6 +156,10 @@ public class InterfazInsertar extends javax.swing.JFrame {
 
         jTReparacion_total.setText("total reparacion");
 
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -161,7 +167,9 @@ public class InterfazInsertar extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jB_menu2))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(31, 31, 31)
@@ -300,9 +308,14 @@ public class InterfazInsertar extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jTReparacion_tiempo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jTReparacion_total, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
-                .addComponent(jB_menu2)
-                .addGap(29, 29, 29))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jB_menu2)
+                        .addGap(29, 29, 29))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -393,6 +406,7 @@ public class InterfazInsertar extends javax.swing.JFrame {
     private javax.swing.JLabel jLvehiculo_marca;
     private javax.swing.JLabel jLvehiculo_modelo;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTCliente_apellido;
     private javax.swing.JTextField jTCliente_dni;
     private javax.swing.JTextField jTCliente_edad;
@@ -404,6 +418,7 @@ public class InterfazInsertar extends javax.swing.JFrame {
     private javax.swing.JTextField jTReparacion_tiempo;
     private javax.swing.JTextField jTReparacion_total;
     private javax.swing.JTextField jTVehiculo_color;
+    private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextField jTvehiculo_año;
     private javax.swing.JTextField jTvehiculo_marca;
     private javax.swing.JTextField jTvehiculo_matricula;
